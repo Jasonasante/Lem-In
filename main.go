8a98a8ef121fd8a4e8c1bb37bd7cabbb5a09ef14
jasonasante <jason.asante@outlook.com>
2022-02-24 20:26:05 +0000

    make a slice of rooms, each containing the name,coordinates, its next and previous rooms, a visited counter and two booleans confirming with the room is the start room or end room through the use of functions.

--- main.go
+++ main.go
@@ -8,94 +8,27 @@ import (
 	"strings"
 )
 
-
-type startEnd struct {
-	start *room 
-	length int
-}
-
 type room struct {
 	name     string
-	nextRoom *room
+	column   int
+	row      int
+	nextRoom []*room
+	prevRoom []*room
+	start    bool
+	end      bool
+	visited  int
 }
 
+var list []*room
 
-//link rooms
-func (s *startEnd) linkRoom(r *room) {
-second:=s.start
-s.start= r
-s.start.nextRoom=second
-s.length++
 
-// var emptyString string
-// var splitLink []string
-
-// r = make([]room, len(s))
-
-// 	for i := 0; i < len(s); i++ {		
-// 		if s[i] == 10 {
-// 			splitLink = strings.Split(emptyString,"-")
-// 			emptyString = ""
-// 			rooms[i].name = string(splitLink[0])
-// 			rooms[i].nextRoom.name= splitLink[1]
-// 		}
-// 		if s[i] != 10 {
-// 			emptyString += string(s[i])
-// 		}
-// 	}
-// fmt.Print(rooms)	
-}
-
-func main() {
+// to initialise rooms with their own address
+func getRooms() {
 	data, _ := os.Open("example.txt")
 	// fmt.Println(data)
 	var emptyString string
-	var links string
-	//this is to get coords by removing # and -
-	getLinks := bufio.NewScanner(data)
-	for getLinks.Scan() {
-		// fmt.Println(scanner.Text())
-		if strings.Contains(getLinks.Text(), "#") {
-			emptyString = ""
-		} else if strings.Contains(getLinks.Text(), " ") {
-			emptyString = ""
-		} else {
-			emptyString = getLinks.Text() + "\n"
-			links += emptyString
-			emptyString = ""
-
-		}
-	}
-	roomsLink := room{}
-	roomsLink.linkRoom(links) 
-
-}
-//find path 
-
-//find shortest route
-
-
-
-
-
-
-
-
-
-
-func visualiser() {
-	var grid [30][30]string
-
-	for row := 0; row < len(grid); row++ {
-		for column := 0; column < len(grid); column++ {
-			grid[row][column] = " "
-		}
-	}
-	data, _ := os.Open("example.txt")
-	// fmt.Println(data)
-	var emptyString string
-	var linkRmv string
-	//this is to get coords by removing # and -
+	var getCoOrd string
+	// this is to get coords by removing # and -
 	getCoOrds := bufio.NewScanner(data)
 	for getCoOrds.Scan() {
 		// fmt.Println(scanner.Text())
@@ -105,42 +38,165 @@ func visualiser() {
 			emptyString = ""
 		} else {
 			emptyString = getCoOrds.Text() + "\n"
-			linkRmv += emptyString
+			getCoOrd += emptyString
 			emptyString = ""
-
 		}
 	}
 	// fmt.Print(linkRmv)
 	var a []string
-	var rooms room
+	var rooms *room
 	var rowInt int
 	var columnInt int
-	//this is to place to rooms on grid
-	for i := 0; i < len(linkRmv); i++ {
-		if linkRmv[i] != 10 {
-			emptyString += string(linkRmv[i])
+
+	// this is to add co-ordinates to their respective room struct
+	for i := 0; i < len(getCoOrd); i++ {
+		if getCoOrd[i] != 10 {
+			emptyString += string(getCoOrd[i])
 		}
-		if linkRmv[i] == 10 {
+		if getCoOrd[i] == 10 {
 			a = strings.Split(emptyString, " ")
 			columnInt, _ = strconv.Atoi(a[1])
 			rowInt, _ = strconv.Atoi(a[2])
-			rooms.name = a[0]
-			for row := 0; row < len(grid); row++ {
-				for column := 0; column < len(grid); column++ {
-					if row == rowInt-1 && column == columnInt-1 {
-						grid[row][column] =  "[" +rooms.name + "]"
-					}
-				}
-			}
+			rooms = &room{name: a[0]}
+			rooms.column = columnInt
+			rooms.row = rowInt
+			list = append(list, rooms)
 			emptyString = ""
 		}
 	}
-	//this is to print grid
+}
+
+//function to assign the start room for ants.
+func assignStart() {
+	data, _ := os.Open("example.txt")
+	var getStart []string
+	var startLine string
+	startInfo := bufio.NewScanner(data)
+	for startInfo.Scan() {
+		getStart = append(getStart, startInfo.Text())
+	}
+
+	for i :=range getStart{
+		if getStart[i]=="##start"{
+			startLine=getStart[i+1]
+		}
+	}
+	a:= strings.Split(startLine," ")
+	for _,ele:=range list{
+		if ele.name==a[0]{
+			ele.start=true
+		}
+	}
+}
+
+//function to assign the end room for ants
+func assignEnd() {
+	data, _ := os.Open("example.txt")
+	var getEnd []string
+	var endLine string
+	// this is to get coords by removing # and -
+	endInfo := bufio.NewScanner(data)
+	for endInfo.Scan() {
+		getEnd = append(getEnd, endInfo.Text())
+	}
+
+	for i :=range getEnd{
+		if getEnd[i]=="##end"{
+			endLine=getEnd[i+1]
+		}
+	}
+	a:= strings.Split(endLine," ")
+	for _,ele:=range list{
+		if ele.name==a[0]{
+			ele.end=true
+		}
+	}
+}
+
+// this links the room to their respective next room(s)
+func linkRooms() {
+	data, _ := os.Open("example.txt")
+	// fmt.Println(data)
+	var emptyString string
+	var links []string
+	// this is to get coords by removing # and -
+	linksInfo := bufio.NewScanner(data)
+	for linksInfo.Scan() {
+		// fmt.Println(scanner.Text())
+		if strings.Contains(linksInfo.Text(), "#") {
+			emptyString = ""
+		} else if strings.Contains(linksInfo.Text(), " ") {
+			emptyString = ""
+		} else {
+			emptyString = linksInfo.Text()
+			links = append(links, emptyString)
+			emptyString = ""
+		}
+	}
+	for i := range links {
+		for j := range links[i] {
+			if links[i][j] == '-' {
+				for k := range list {
+					for o := range list {
+						if string(links[i][j-1]) == list[k].name && list[o].name == string(links[i][j+1]) {
+							length := len(list[k].nextRoom)
+							list[k].nextRoom = append(list[k].nextRoom, list[o])
+							list[k].nextRoom[length].prevRoom = append(list[k].nextRoom[length].prevRoom, list[k])
+						} else if string(links[i][j+1]) == list[k].name && list[o].name == string(links[i][j-1]) {
+							length := len(list[k].nextRoom)
+							list[k].nextRoom = append(list[k].nextRoom, list[o])
+							list[k].nextRoom[length].prevRoom = append(list[k].nextRoom[length].prevRoom, list[k])
+						}
+					}
+				}
+			}
+		}
+	}
+	// for _, ele := range list {
+	// 	fmt.Println("list of rooms", *ele)
+	// 	for _, room := range ele.prevRoom {
+	// 		fmt.Println(*room)
+	// 	}
+	// }
+}
+
+// find path
+
+
+// find shortest route
+
+// place rooms in grid
+func grid() {
+	var grid [30][30]string
+	for row := 0; row < len(grid); row++ {
+		for column := 0; column < len(grid); column++ {
+			grid[row][column] = " "
+		}
+	}
+	for i := range list {
+		for row := 0; row < len(grid); row++ {
+			for column := 0; column < len(grid); column++ {
+				if row == list[i].row-1 && column == list[i].column-1 {
+					grid[row][column] = "[" + list[i].name + "]"
+				}
+			}
+		}
+	}
 	for i := range grid {
 		for _, ele := range grid[i] {
 			fmt.Print(ele)
 		}
 		fmt.Println()
 	}
-
 }
+
+func main() {
+	getRooms()
+	assignStart()
+	assignEnd()
+	linkRooms()
+}
+
+// find path
+
+// find shortest route
